ff67279840d811436bf49ded5c85e94f52e25c12
sgretscher <41306803+sgretscher@users.noreply.github.com>
2022-06-20 20:05:18 +0200

    IPCDemo: per default, block sender until receiver has handled message (even if no reply is desired)
    Since the plug-in will make callbacks from several host calls, the host must wait until these have been done before proceeding.

--- IPCDemo/IPCPort.cpp
+++ IPCDemo/IPCPort.cpp
@@ -24,8 +24,12 @@
 #include "IPCPort.h"
 #include "ARA_Library/Debug/ARADebug.h"
 
-// increase to several seconds while debugging so that staying in the debugger does not break program flow
-static constexpr auto messageTimeout { 0.1 };
+#if defined(NDEBUG)
+    constexpr auto messageTimeout { 0.5 };
+#else
+    // increase to 5 min while debugging so that stopping in the debugger does not break program flow
+    constexpr auto messageTimeout { 5 * 60.0 };
+#endif
 
 IPCPort::IPCPort (CFMessagePortRef port) :
     _port { port }
@@ -53,6 +57,8 @@ IPCPort::~IPCPort ()
 
 CFDataRef IPCPortCallBack (CFMessagePortRef /*port*/, SInt32 msgid, CFDataRef cfData, void* info)
 {
+//  ARA_LOG ("IPCPortCallBack %i", msgid);
+
     const IPCMessage message { cfData };
     return ((IPCPort::Callback) info) (msgid, message).createEncodedMessage ();
 }
@@ -94,23 +100,42 @@ IPCPort IPCPort::createConnectedToID (const char* remotePortID)
     return IPCPort { port };
 }
 
-void IPCPort::sendWithoutReply (const int32_t messageID, const IPCMessage& message)
+void IPCPort::sendNonblocking (const int32_t messageID, const IPCMessage& message)
 {
+//  ARA_LOG ("IPCPort::sendNonblocking %i", messageID);
+
     auto outgoingData { message.createEncodedMessage () };
     const auto portSendResult { CFMessagePortSendRequest (_port, messageID, outgoingData, messageTimeout, 0.0, nullptr, nullptr) };
-    CFRelease (outgoingData);
+    if (outgoingData)
+        CFRelease (outgoingData);
     ARA_INTERNAL_ASSERT (portSendResult == kCFMessagePortSuccess);
+}
 
+void IPCPort::sendBlocking (const int32_t messageID, const IPCMessage& message)
+{
+//  ARA_LOG ("IPCPort::sendBlocking %i", messageID);
+
+    auto incomingData { _sendBlocking (messageID, message) };
+    CFRelease (incomingData);
 }
 
 IPCMessage IPCPort::sendAndAwaitReply (const int32_t messageID, const IPCMessage& message)
 {
-    auto outgoingData { message.createEncodedMessage () };
-    auto incomingData { CFDataRef {} };
-    const auto portSendResult { CFMessagePortSendRequest (_port, messageID, outgoingData, messageTimeout, messageTimeout, kCFRunLoopDefaultMode, &incomingData) };
-    CFRelease (outgoingData);
-    ARA_INTERNAL_ASSERT (incomingData && (portSendResult == kCFMessagePortSuccess));
+//  ARA_LOG ("IPCPort::sendAndAwaitReply %i", messageID);
+
+    auto incomingData { _sendBlocking (messageID, message) };
     IPCMessage reply { incomingData };
     CFRelease (incomingData);
     return reply;
+}
+
+CFDataRef IPCPort::_sendBlocking (const int32_t messageID, const IPCMessage& message)
+{
+    CFDataRef incomingData {};
+    auto outgoingData { message.createEncodedMessage () };
+    const auto portSendResult { CFMessagePortSendRequest (_port, messageID, outgoingData, messageTimeout, messageTimeout, kCFRunLoopDefaultMode, &incomingData) };
+    if (outgoingData)
+        CFRelease (outgoingData);
+    ARA_INTERNAL_ASSERT (incomingData && (portSendResult == kCFMessagePortSuccess));
+    return incomingData;
 }
