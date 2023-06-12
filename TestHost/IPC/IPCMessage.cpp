b962bdc281e17a157d2c2aee9789db2b0d0a4816
sgretscher <41306803+sgretscher@users.noreply.github.com>
2023-06-12 22:29:56 +0200

    Add pugixml-based implementation to IPCMessage for non-Apple platforms

--- TestHost/IPC/IPCMessage.cpp
+++ TestHost/IPC/IPCMessage.cpp
@@ -22,10 +22,17 @@
 #include <limits>
 #include <string>
 #include <set>
+#include <sstream>
 #include <map>
 
+#if defined (__APPLE__)
 _Pragma ("GCC diagnostic push")
 _Pragma ("GCC diagnostic ignored \"-Wold-style-cast\"")
+#endif
+
+//------------------------------------------------------------------------------
+#if IPC_MESSAGE_USE_CFDICTIONARY
+//------------------------------------------------------------------------------
 
 class _CFReleaser
 {
@@ -51,16 +58,6 @@ void IPCMessage::_setup (CFDictionaryRef dictionary, bool isWritable)
     else
         _dictionary = CFDictionaryCreateMutableCopy (kCFAllocatorDefault, 0, dictionary);
     _isWritable = isWritable;
-}
-
-IPCMessage::IPCMessage (const IPCMessage& other)
-{
-    *this = other;
-}
-
-IPCMessage::IPCMessage (IPCMessage&& other) noexcept
-{
-    *this = std::move (other);
 }
 
 IPCMessage& IPCMessage::operator= (const IPCMessage& other)
@@ -91,7 +88,7 @@ CFStringRef IPCMessage::_getEncodedKey (size_t argKey)
     auto existingEntry { cache.find (argKey) };
     if (existingEntry != cache.end ())
         return existingEntry->second;
-    return  cache.emplace (argKey, CFStringCreateWithCString (kCFAllocatorDefault, std::to_string (argKey).c_str (), kCFStringEncodingUTF8)).first->second;
+    return cache.emplace (argKey, CFStringCreateWithCString (kCFAllocatorDefault, std::to_string (argKey).c_str (), kCFStringEncodingUTF8)).first->second;
 }
 
 IPCMessage::IPCMessage (CFDataRef data)
@@ -116,7 +113,7 @@ void IPCMessage::_appendArg (size_t argKey, int64_t argValue)
 
 void IPCMessage::_appendArg (size_t argKey, size_t argValue)
 {
-    static_assert (sizeof (SInt64) == sizeof (size_t), "currently only implemented for 64 bit");
+    static_assert (sizeof (SInt64) == sizeof (size_t), "integer type needs adjustment for this compiler setup");
     _appendEncodedArg (argKey, CFNumberCreate (kCFAllocatorDefault, kCFNumberSInt64Type, &argValue));
 }
 
@@ -199,11 +196,11 @@ void IPCMessage::_readArg (size_t argKey, int64_t& argValue, bool* didFindKey) c
 void IPCMessage::_readArg (size_t argKey, size_t& argValue, bool* didFindKey) const
 {
     ARA_INTERNAL_ASSERT (_dictionary);
-    static_assert (sizeof (SInt64) == sizeof (size_t), "currently only implemented for 64 bit");
+    static_assert (sizeof (SInt64) == sizeof (size_t), "integer type needs adjustment for this compiler setup");
     auto number { (CFNumberRef) CFDictionaryGetValue (_dictionary, _getEncodedKey (argKey)) };
     if (!_checkKeyFound (number != nullptr, didFindKey))
     {
-        argValue = 0;
+        argValue = 0U;
         return;
     }
     ARA_INTERNAL_ASSERT (number && (CFGetTypeID (number) == CFNumberGetTypeID ()));
@@ -288,14 +285,282 @@ void IPCMessage::_readArg (size_t argKey, IPCMessage& argValue, bool* didFindKey
     argValue._setup (dictionary, false);
 }
 
-bool IPCMessage::_checkKeyFound (bool found, bool* didFindKey)
+//------------------------------------------------------------------------------
+#else   // IPC_MESSAGE_USE_CFDICTIONARY
+//------------------------------------------------------------------------------
+
+constexpr auto kRootKey { "msg" };
+
+IPCMessage& IPCMessage::operator= (const IPCMessage& other)
 {
-    if (didFindKey)
+    if (other._isWritable)
     {
-        *didFindKey = found;
-        return found;
+        _dictionary.reset (new pugi::xml_document);
+        _root = _dictionary->append_copy (other._root);
+        _root.set_name (kRootKey);
+        _isWritable = true;
     }
-    return true;
+    else
+    {
+        _dictionary = other._dictionary;
+        _root = other._root;
+        _isWritable = false;
+    }
+
+    return *this;
 }
 
+IPCMessage& IPCMessage::operator= (IPCMessage&& other) noexcept
+{
+    std::swap (_dictionary, other._dictionary);
+    std::swap (_root, other._root);
+    std::swap (_isWritable, other._isWritable);
+    return *this;
+}
+
+const char* IPCMessage::_getEncodedKey (size_t argKey)
+{
+    static std::map<size_t, std::string> cache;
+    auto existingEntry { cache.find (argKey) };
+    if (existingEntry != cache.end ())
+        return existingEntry->second.c_str ();
+    // \todo pugixml ignores attributes with only numbers as keys - bug or feature? for now, we just prepend an underscore.
+    return cache.emplace (argKey, std::string { "_"} + std::to_string (argKey)).first->second.c_str ();
+}
+
+#if defined (__APPLE__)
+IPCMessage::IPCMessage (CFDataRef dataObject)
+#else
+IPCMessage::IPCMessage (const char* data, size_t dataSize)
+#endif
+{
+    _dictionary.reset (new pugi::xml_document);
+#if defined (__APPLE__)
+    const auto data { CFDataGetBytePtr (dataObject) };
+    const auto dataSize { (size_t) CFDataGetLength (dataObject) };
+#endif
+    _dictionary->load_buffer (data, dataSize, pugi::parse_minimal | pugi::parse_escapes, pugi::encoding_utf8);
+    _root = _dictionary->child (kRootKey);
+    _isWritable = false;
+}
+
+void IPCMessage::_appendArg (size_t argKey, int32_t argValue)
+{
+    _appendAttribute (argKey).set_value (argValue);
+}
+
+void IPCMessage::_appendArg (size_t argKey, int64_t argValue)
+{
+    _appendAttribute (argKey).set_value (argValue);
+}
+
+void IPCMessage::_appendArg (size_t argKey, size_t argValue)
+{
+    _appendAttribute (argKey).set_value (argValue);
+}
+
+void IPCMessage::_appendArg (size_t argKey, float argValue)
+{
+    _appendAttribute (argKey).set_value (argValue);
+}
+
+void IPCMessage::_appendArg (size_t argKey, double argValue)
+{
+    _appendAttribute (argKey).set_value (argValue);
+}
+
+void IPCMessage::_appendArg (size_t argKey, const char* argValue)
+{
+    _appendAttribute (argKey).set_value (argValue);
+}
+
+void IPCMessage::_appendArg (size_t argKey, const std::vector<uint8_t>& argValue)
+{
+    const auto encodedData { base64_encode (argValue.data (), argValue.size (), false) };
+    _appendAttribute (argKey).set_value (encodedData.c_str ());
+}
+
+pugi::xml_attribute IPCMessage::_appendAttribute (size_t argKey)
+{
+    _makeWritableIfNeeded ();
+    return _root.append_attribute (_getEncodedKey (argKey));
+}
+
+void IPCMessage::_appendArg (int32_t argKey, const IPCMessage& argValue)
+{
+    _makeWritableIfNeeded ();
+    _root.append_copy (argValue._root).set_name (_getEncodedKey (argKey));
+}
+
+void IPCMessage::_makeWritableIfNeeded ()
+{
+    if (_isWritable)
+        return;
+
+    pugi::xml_document* dictionary { new pugi::xml_document };
+    if (_root.empty ())
+    {
+        _root = dictionary->append_child (kRootKey);
+    }
+    else
+    {
+        _root = dictionary->append_copy (_root);
+        _root.set_name (kRootKey);
+    }
+    _dictionary.reset (dictionary);
+    _isWritable = true;
+}
+
+#if defined (__APPLE__)
+CFDataRef IPCMessage::createEncodedMessage () const
+#else
+std::string IPCMessage::createEncodedMessage () const
+#endif
+{
+    if (_root.empty ())
+#if defined (__APPLE__)
+        return nullptr;
+#else
+        return {};
+#endif
+
+    auto dictionary { _dictionary };
+    if (_root != _dictionary->child (kRootKey))
+    {
+        dictionary.reset (new pugi::xml_document);
+        dictionary->append_child (kRootKey).append_copy (_root);
+    }
+
+    std::ostringstream writer;
+    dictionary->save (writer, "", pugi::format_raw | pugi::format_no_declaration);
+
+#if defined (__APPLE__)
+    auto result { CFDataCreate (kCFAllocatorDefault, (const UInt8*) writer.str ().c_str (), (CFIndex) writer.str ().size ()) };
+    ARA_INTERNAL_ASSERT (result);
+    return result;
+#else
+    return writer.str();
+#endif
+}
+
+void IPCMessage::_readArg (size_t argKey, int32_t& argValue, bool* didFindKey) const
+{
+    ARA_INTERNAL_ASSERT (!_root.empty ());
+    const auto attribute { _root.attribute (_getEncodedKey (argKey)) };
+    if (!_checkKeyFound (!attribute.empty (), didFindKey))
+    {
+        argValue = 0;
+        return;
+    }
+    static_assert (sizeof (int) >= sizeof (int32_t), "integer type needs adjustment for this compiler setup");
+    argValue = attribute.as_int ();
+}
+
+void IPCMessage::_readArg (size_t argKey, int64_t& argValue, bool* didFindKey) const
+{
+    ARA_INTERNAL_ASSERT (!_root.empty ());
+    const auto attribute { _root.attribute (_getEncodedKey (argKey)) };
+    if (!_checkKeyFound (!attribute.empty (), didFindKey))
+    {
+        argValue = 0;
+        return;
+    }
+    static_assert (sizeof (long long) >= sizeof (int64_t), "integer type needs adjustment for this compiler setup");
+    argValue = attribute.as_llong ();
+}
+
+void IPCMessage::_readArg (size_t argKey, size_t& argValue, bool* didFindKey) const
+{
+    ARA_INTERNAL_ASSERT (!_root.empty ());
+    const auto attribute { _root.attribute (_getEncodedKey (argKey)) };
+    if (!_checkKeyFound (!attribute.empty (), didFindKey))
+    {
+        argValue = 0U;
+        return;
+    }
+    static_assert (sizeof (unsigned long long) >= sizeof (size_t), "integer type needs adjustment for this compiler setup");
+    argValue = attribute.as_ullong ();
+}
+
+void IPCMessage::_readArg (size_t argKey, float& argValue, bool* didFindKey) const
+{
+    ARA_INTERNAL_ASSERT (!_root.empty ());
+    const auto attribute { _root.attribute (_getEncodedKey (argKey)) };
+    if (!_checkKeyFound (!attribute.empty (), didFindKey))
+    {
+        argValue = 0.0f;
+        return;
+    }
+    argValue = attribute.as_float ();
+}
+
+void IPCMessage::_readArg (size_t argKey, double& argValue, bool* didFindKey) const
+{
+    ARA_INTERNAL_ASSERT (!_root.empty ());
+    const auto attribute { _root.attribute (_getEncodedKey (argKey)) };
+    if (!_checkKeyFound (!attribute.empty (), didFindKey))
+    {
+        argValue = 0.0;
+        return;
+    }
+    argValue = attribute.as_double ();
+}
+
+void IPCMessage::_readArg (size_t argKey, const char*& argValue, bool* didFindKey) const
+{
+    ARA_INTERNAL_ASSERT (!_root.empty ());
+    const auto attribute { _root.attribute (_getEncodedKey (argKey)) };
+    if (!_checkKeyFound (!attribute.empty (), didFindKey))
+    {
+        argValue = nullptr;
+        return;
+    }
+    argValue = attribute.as_string ();
+}
+
+void IPCMessage::_readArg (size_t argKey, std::vector<uint8_t>& argValue, bool* didFindKey) const
+{
+    ARA_INTERNAL_ASSERT (!_root.empty ());
+    const auto attribute { _root.attribute (_getEncodedKey (argKey)) };
+    if (!_checkKeyFound (!attribute.empty (), didFindKey))
+    {
+        argValue.clear ();
+        return;
+    }
+
+    const auto decodedData { base64_decode (attribute.as_string (), false) };
+    argValue.assign ((const uint8_t*) decodedData.c_str (), (const uint8_t*) decodedData.c_str () + decodedData.size ());
+}
+
+void IPCMessage::_readArg (size_t argKey, IPCMessage& argValue, bool* didFindKey) const
+{
+    ARA_INTERNAL_ASSERT (!_root.empty ());
+    const auto child { _root.child (_getEncodedKey (argKey)) };
+    if (!_checkKeyFound (!child.empty (), didFindKey))
+    {
+        argValue = {};
+        return;
+    }
+
+    if (_isWritable)
+    {
+        argValue._dictionary.reset (new pugi::xml_document);
+        argValue._root = argValue._dictionary->append_copy (child);
+        argValue._root.set_name (kRootKey);
+        argValue._isWritable = true;
+    }
+    else
+    {
+        argValue._dictionary = _dictionary;
+        argValue._root = child;
+        argValue._isWritable = false;
+    }
+}
+
+//------------------------------------------------------------------------------
+#endif  // IPC_MESSAGE_USE_CFDICTIONARY
+//------------------------------------------------------------------------------
+
+#if defined (__APPLE__)
 _Pragma ("GCC diagnostic pop")
+#endif
