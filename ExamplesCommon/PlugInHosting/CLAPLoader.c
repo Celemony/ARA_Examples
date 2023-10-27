4d83c71850207851b54c4d41d384af412f8fa2dd
sgretscher <41306803+sgretscher@users.noreply.github.com>
2023-10-27 10:34:09 +0200

    Add CLAP loader for example hosts

---
+++ ExamplesCommon/PlugInHosting/CLAPLoader.c
@@ -0,0 +1,398 @@
+//------------------------------------------------------------------------------
+//! \file       CLAPLoader.c
+//!             CLAP specific ARA implementation for the SDK's hosting examples
+//! \project    ARA SDK Examples
+//! \copyright  Copyright (c) 2022-2022, Celemony Software GmbH, All Rights Reserved.
+//!             Developed in cooperation with Timo Kaluza (defiantnerd)
+//! \license    Licensed under the Apache License, Version 2.0 (the "License");
+//!             you may not use this file except in compliance with the License.
+//!             You may obtain a copy of the License at
+//!
+//!               http://www.apache.org/licenses/LICENSE-2.0
+//!
+//!             Unless required by applicable law or agreed to in writing, software
+//!             distributed under the License is distributed on an "AS IS" BASIS,
+//!             WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//!             See the License for the specific language governing permissions and
+//!             limitations under the License.
+//------------------------------------------------------------------------------
+
+#include "CLAPLoader.h"
+
+#include "ARA_API/ARACLAP.h"
+#include "ARA_Library/Debug/ARADebug.h"
+
+#if defined(_WIN32)
+    #define WIN32_LEAN_AND_MEAN
+    #include <Windows.h>
+    #include <conio.h>
+#elif defined(__APPLE__)
+    #include <CoreFoundation/CoreFoundation.h>
+#elif defined(__linux__)
+    #include <dlfcn.h>
+#endif
+
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#if defined(_MSC_VER)
+   __pragma(warning(disable : 4100)) /*unreferenced formal parameter*/
+#elif defined(__GNUC__)
+   _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")
+#endif
+
+
+struct _CLAPBinary
+{
+#if defined(_WIN32)
+    HMODULE libHandle;
+#elif defined(__APPLE__)
+    CFBundleRef libHandle;
+#elif defined(__linux__)
+    void* libHandle;
+#endif
+    const clap_plugin_entry_t * entry;
+};
+
+struct _CLAPPlugIn
+{
+    const clap_plugin_t * plugin;
+    double sampleRate;
+};
+
+
+CLAPBinary CLAPLoadBinary(const char * binaryName)
+{
+#if defined (__APPLE__) || defined (__linux__)
+    bool freeBinaryName = false;
+    // if the binary name contains no '/', dlopen() searches the system library paths for the file,
+    // and ignores the current directory - to prevent this, we prefix with "./" if needed
+    if (!strchr(binaryName, '/'))
+    {
+        const char * prefix = "./";
+        const size_t prefixLength = strlen(prefix);
+        const size_t binaryNameLength = strlen(binaryName);
+        char * temp = (char *)malloc(prefixLength + binaryNameLength + 1);
+        ARA_INTERNAL_ASSERT(temp);
+        memcpy(temp, prefix, prefixLength);
+        memcpy(temp + prefixLength, binaryName, binaryNameLength + 1);  // including termination
+        binaryName = temp;
+        freeBinaryName = true;
+    }
+#endif
+
+    CLAPBinary clapBinary = malloc(sizeof(struct _CLAPBinary));
+    ARA_INTERNAL_ASSERT(clapBinary);
+    clapBinary->libHandle = NULL;
+    clapBinary->entry = NULL;
+
+#if defined (_WIN32)
+    clapBinary->libHandle = LoadLibraryA(binaryName);
+    ARA_INTERNAL_ASSERT(clapBinary->libHandle);
+
+    clapBinary->entry = (const clap_plugin_entry_t *)GetProcAddress(clapBinary->libHandle, "clap_entry");
+
+#elif defined(__APPLE__)
+    CFURLRef url = CFURLCreateFromFileSystemRepresentation(kCFAllocatorDefault, (const UInt8 *)binaryName, (CFIndex)strlen(binaryName), true);
+    ARA_INTERNAL_ASSERT (url);
+    clapBinary->libHandle = CFBundleCreate(kCFAllocatorDefault, url);
+    CFRelease (url);
+    ARA_INTERNAL_ASSERT(clapBinary->libHandle);
+
+    Boolean ARA_MAYBE_UNUSED_VAR(didLoad);
+    didLoad = CFBundleLoadExecutable(clapBinary->libHandle);
+    ARA_INTERNAL_ASSERT(didLoad);
+
+    clapBinary->entry = (const clap_plugin_entry_t *)CFBundleGetFunctionPointerForName(clapBinary->libHandle, CFSTR("clap_entry"));
+
+#elif defined(__linux__)
+    clapBinary->libHandle = dlopen(binaryName, RTLD_LOCAL | RTLD_LAZY);
+    ARA_INTERNAL_ASSERT(clapBinary->libHandle);
+
+    clapBinary->entry = (const clap_plugin_entry_t *)dlsym(clapBinary->libHandle, "clap_entry");
+#endif
+
+    ARA_INTERNAL_ASSERT(clapBinary->entry != NULL);
+    ARA_INTERNAL_ASSERT(clap_version_is_compatible(clapBinary->entry->clap_version));
+
+    bool success = clapBinary->entry->init(binaryName);
+    ARA_INTERNAL_ASSERT(success);
+
+#if defined (__APPLE__) || defined (__linux__)
+    if (freeBinaryName)
+        free((char *)binaryName);
+#endif
+
+    return clapBinary;
+}
+
+const ARAFactory * CLAPGetARAFactory(CLAPBinary clapBinary, const char * optionalPlugInName)
+{
+    const clap_ara_factory_t * ara_factory = (const clap_ara_factory_t *)clapBinary->entry->get_factory(CLAP_EXT_ARA_FACTORY);
+    if (ara_factory == NULL)
+        return NULL;
+
+    const uint32_t plugin_count = ara_factory->get_factory_count(ara_factory);
+    ARA_INTERNAL_ASSERT(plugin_count > 0);
+
+    if (optionalPlugInName)
+    {
+        for (uint32_t i = 0; i < plugin_count; ++i)
+        {
+            const ARAFactory * factory = ara_factory->get_ara_factory(ara_factory, i);
+            ARA_INTERNAL_ASSERT(factory != NULL);
+            if (strcmp(optionalPlugInName, factory->plugInName) == 0)
+                return factory;
+        }
+        return NULL;
+    }
+    else
+    {
+        const ARAFactory * factory =  ara_factory->get_ara_factory(ara_factory, 0);
+        ARA_INTERNAL_ASSERT(factory != NULL);
+        return factory;
+    }
+}
+
+const void * host_get_extension(const clap_host_t * ARA_MAYBE_UNUSED_ARG(host), const char * ARA_MAYBE_UNUSED_ARG(extension_id))
+{
+    return NULL;
+}
+
+void host_request_dummy(const clap_host_t * ARA_MAYBE_UNUSED_ARG(host))
+{
+}
+
+#define _IN_QUOTES_HELPER(x) #x
+#define IN_QUOTES(x) _IN_QUOTES_HELPER(x)
+static const clap_host_t clap_host =
+{
+    .clap_version = CLAP_VERSION_INIT,
+    .host_data = NULL,
+    .name = "ARA SDK Host Examples",
+    .vendor = "ARA Demo Company",
+    .url =  "https://www.arademocompany.com",
+    .version = IN_QUOTES(ARA_MAJOR_VERSION) "." IN_QUOTES(ARA_MINOR_VERSION) "." IN_QUOTES(ARA_PATCH_VERSION),
+    .get_extension = host_get_extension,
+    .request_restart = host_request_dummy,
+    .request_process = host_request_dummy,
+    .request_callback = host_request_dummy
+};
+#undef _IN_QUOTES_HELPER
+#undef IN_QUOTES
+
+CLAPPlugIn CLAPCreatePlugIn(CLAPBinary clapBinary, const char* optionalPlugInName)
+{
+    const clap_plugin_factory_t * factory = (const clap_plugin_factory_t *)clapBinary->entry->get_factory(CLAP_PLUGIN_FACTORY_ID);
+    ARA_INTERNAL_ASSERT(factory != NULL);
+
+    const uint32_t plugin_count = factory->get_plugin_count(factory);
+    ARA_INTERNAL_ASSERT(plugin_count > 0);
+
+    const clap_plugin_descriptor_t * desc = NULL;
+    if (optionalPlugInName)
+    {
+        for (uint32_t i = 0; i < plugin_count; ++i)
+        {
+            const clap_plugin_descriptor_t * descAtIndex = factory->get_plugin_descriptor(factory, i);
+            ARA_INTERNAL_ASSERT(descAtIndex != NULL);
+            if (strcmp(optionalPlugInName, descAtIndex->name) == 0)
+            {
+                desc = descAtIndex;
+                break;
+            }
+        }
+        if (desc == NULL)
+            return NULL;
+    }
+    else
+    {
+        desc = factory->get_plugin_descriptor(factory, 0);
+        ARA_INTERNAL_ASSERT(desc != NULL);
+    }
+
+    CLAPPlugIn clapPlugIn = malloc(sizeof(struct _CLAPPlugIn));
+    ARA_INTERNAL_ASSERT(clapPlugIn);
+    clapPlugIn->plugin = factory->create_plugin(factory, &clap_host, desc->id);
+    clapPlugIn->plugin->init(clapPlugIn->plugin);
+    return clapPlugIn;
+}
+
+const ARAPlugInExtensionInstance * CLAPBindToARADocumentController(CLAPPlugIn clapPlugIn, ARADocumentControllerRef controllerRef, ARAPlugInInstanceRoleFlags assignedRoles)
+{
+    const clap_ara_plugin_extension_t * ara_extension = clapPlugIn->plugin->get_extension(clapPlugIn->plugin, CLAP_EXT_ARA_PLUGINEXTENSION);
+    if (ara_extension == NULL)
+        return NULL;
+
+    const ARAPlugInInstanceRoleFlags knownRoles = kARAPlaybackRendererRole | kARAEditorRendererRole | kARAEditorViewRole;
+    ARA_INTERNAL_ASSERT((assignedRoles | knownRoles) == knownRoles);
+    ARA_VALIDATE_API_CONDITION(!strcmp(clapPlugIn->plugin->desc->name, ara_extension->get_factory(clapPlugIn->plugin)->plugInName));
+    return ara_extension->bind_to_document_controller(clapPlugIn->plugin, controllerRef, knownRoles, assignedRoles);
+}
+
+void CLAPStartRendering(CLAPPlugIn clapPlugIn, uint32_t maxBlockSize, double sampleRate)
+{
+    bool ARA_MAYBE_UNUSED_VAR(success);
+
+    // we require that ARA plug-ins are capable of handling mono and stereo
+    const clap_plugin_audio_ports_config_t * audio_ports_config = clapPlugIn->plugin->get_extension(clapPlugIn->plugin, CLAP_EXT_AUDIO_PORTS_CONFIG);
+    ARA_INTERNAL_ASSERT(audio_ports_config);
+    uint32_t count = audio_ports_config->count(clapPlugIn->plugin);
+    ARA_INTERNAL_ASSERT(count >= 2);
+    // this simple loader always uses mono, pick appropriate config
+    bool ARA_MAYBE_UNUSED_VAR(foundMonoConfig);
+    foundMonoConfig = false;
+    for (uint32_t i = 0; i < count; ++i)
+    {
+        clap_audio_ports_config_t config;
+        success = audio_ports_config->get(clapPlugIn->plugin, i, &config);
+        ARA_INTERNAL_ASSERT(success);
+        if (config.has_main_input &&
+            config.has_main_output &&
+            config.main_input_channel_count == 1 &&
+            config.main_output_channel_count == 1)
+        {
+            success = audio_ports_config->select(clapPlugIn->plugin, config.id);
+            ARA_INTERNAL_ASSERT(success);
+            foundMonoConfig = true;
+            break;
+        }
+    }
+    ARA_INTERNAL_ASSERT(foundMonoConfig);
+
+    // validate audio port info
+    const clap_plugin_audio_ports_t * audio_ports = clapPlugIn->plugin->get_extension(clapPlugIn->plugin, CLAP_EXT_AUDIO_PORTS);
+    ARA_INTERNAL_ASSERT(audio_ports);
+    ARA_INTERNAL_ASSERT(audio_ports->count(clapPlugIn->plugin, true) > 0);
+    ARA_INTERNAL_ASSERT(audio_ports->count(clapPlugIn->plugin, false) > 0);
+    clap_audio_port_info_t in_info, out_info;
+    success = audio_ports->get(clapPlugIn->plugin, 0, true, &in_info);
+    ARA_INTERNAL_ASSERT(success);
+    success = audio_ports->get(clapPlugIn->plugin, 0, false, &out_info);
+    ARA_INTERNAL_ASSERT(success);
+    // we require that ARA plug-ins are capable of in-place processing (typically used for editor rendering)
+    ARA_INTERNAL_ASSERT(in_info.in_place_pair == out_info.id);
+    ARA_INTERNAL_ASSERT(in_info.id == out_info.in_place_pair);
+    // this simple loader only supports mono
+    ARA_INTERNAL_ASSERT(in_info.channel_count == 1);
+    ARA_INTERNAL_ASSERT(out_info.channel_count == 1);
+
+    clapPlugIn->sampleRate = sampleRate;
+
+    clapPlugIn->plugin->activate(clapPlugIn->plugin, sampleRate, 1, maxBlockSize);
+    clapPlugIn->plugin->start_processing(clapPlugIn->plugin);
+}
+
+uint32_t CLAP_ABI input_events_size(const struct clap_input_events* list)
+{
+    return 0;
+}
+
+const clap_event_header_t* CLAP_ABI input_events_get(const struct clap_input_events* list, uint32_t index)
+{
+    return NULL;
+}
+
+bool CLAP_ABI output_events_try_push(const struct clap_output_events* list, const clap_event_header_t* event)
+{
+  return false;
+}
+
+void CLAPRenderBuffer(CLAPPlugIn clapPlugIn, uint32_t blockSize, int64_t samplePosition, float * buffer)
+{
+    ARA_INTERNAL_ASSERT(blockSize >= 1);
+
+    // events
+    const clap_input_events_t input_events =
+    {
+        .ctx = NULL, // reserved pointer for the list
+        .size = input_events_size,
+        .get = input_events_get
+    };
+
+    clap_output_events_t output_events =
+    {
+        .ctx = NULL,
+        .try_push = output_events_try_push
+    };
+
+    // transport
+    const clap_event_transport_t transport =
+    {
+        .header =
+        {
+            .type = CLAP_EVENT_TRANSPORT,
+            .space_id = CLAP_CORE_EVENT_SPACE_ID,
+            .size = sizeof(clap_event_transport_t),
+            .time = 0,
+            .flags = 0,
+        },
+        .flags = CLAP_TRANSPORT_HAS_SECONDS_TIMELINE | CLAP_TRANSPORT_IS_PLAYING,
+        .song_pos_seconds = (int64_t)round(((double)CLAP_SECTIME_FACTOR) * (((double)samplePosition) / clapPlugIn->sampleRate))
+    };
+
+    // I/O
+    const clap_audio_buffer_t audio_inputs =
+    {
+        .data32 = &buffer,
+        .channel_count = 1,
+        .latency = 0,
+        .constant_mask = UINT64_MAX
+    };
+
+    clap_audio_buffer_t audio_outputs =
+    {
+        .data32 = &buffer,
+        .channel_count = 1,
+        .latency = 0,
+        .constant_mask = 0
+    };
+
+    memset(buffer, 0, blockSize * sizeof(float));
+
+    // process
+    clap_process_t process =
+    {
+        .steady_time = -1,
+        .frames_count = blockSize,
+        .transport = &transport,
+        .audio_inputs = &audio_inputs,
+        .audio_outputs = &audio_outputs,
+        .audio_inputs_count = 1,
+        .audio_outputs_count = 1,
+        .in_events = &input_events,
+        .out_events = &output_events
+    };
+
+    clap_process_status ARA_MAYBE_UNUSED_VAR(status);
+    status = clapPlugIn->plugin->process(clapPlugIn->plugin, &process);
+    ARA_INTERNAL_ASSERT(status != CLAP_PROCESS_ERROR);
+}
+
+void CLAPStopRendering(CLAPPlugIn clapPlugIn)
+{
+    clapPlugIn->plugin->stop_processing(clapPlugIn->plugin);
+    clapPlugIn->plugin->deactivate(clapPlugIn->plugin);
+}
+
+void CLAPDestroyPlugIn(CLAPPlugIn clapPlugIn)
+{
+    clapPlugIn->plugin->destroy(clapPlugIn->plugin);
+    free(clapPlugIn);
+}
+
+void CLAPUnloadBinary(CLAPBinary clapBinary)
+{
+    clapBinary->entry->deinit();
+
+#if defined(_WIN32)
+    FreeLibrary(clapBinary->libHandle);
+#elif defined(__APPLE__)
+    CFRelease(clapBinary->libHandle);
+#elif defined(__linux__)
+    dlclose(clapBinary->libHandle);
+#endif
+    free(clapBinary);
+}
---
+++ ExamplesCommon/PlugInHosting/CLAPLoader.h
@@ -0,0 +1,47 @@
+//------------------------------------------------------------------------------
+//! \file       CLAPLoader.h
+//!             CLAP specific ARA implementation for the SDK's hosting examples
+//! \project    ARA SDK Examples
+//! \copyright  Copyright (c) 2022-2022, Celemony Software GmbH, All Rights Reserved.
+//!             Developed in cooperation with Timo Kaluza (defiantnerd)
+//! \license    Licensed under the Apache License, Version 2.0 (the "License");
+//!             you may not use this file except in compliance with the License.
+//!             You may obtain a copy of the License at
+//!
+//!               http://www.apache.org/licenses/LICENSE-2.0
+//!
+//!             Unless required by applicable law or agreed to in writing, software
+//!             distributed under the License is distributed on an "AS IS" BASIS,
+//!             WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//!             See the License for the specific language governing permissions and
+//!             limitations under the License.
+//------------------------------------------------------------------------------
+
+#pragma once
+
+#include "ARA_API/ARAInterface.h"
+#include "ARA_Library/Debug/ARADebug.h"
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+struct _CLAPBinary;
+struct _CLAPPlugIn;
+typedef struct _CLAPBinary * CLAPBinary;
+typedef struct _CLAPPlugIn * CLAPPlugIn;
+
+CLAPBinary CLAPLoadBinary(const char * binaryName);
+const ARA_NAMESPACE ARAFactory * CLAPGetARAFactory(CLAPBinary clapBinary, const char * optionalPlugInName);
+CLAPPlugIn CLAPCreatePlugIn(CLAPBinary clapBinary, const char * optionalPlugInName);
+const ARA_NAMESPACE ARAPlugInExtensionInstance * CLAPBindToARADocumentController(CLAPPlugIn clapPlugIn, ARA_NAMESPACE ARADocumentControllerRef controllerRef, ARA_NAMESPACE ARAPlugInInstanceRoleFlags assignedRoles);
+void CLAPStartRendering(CLAPPlugIn clapPlugIn, uint32_t maxBlockSize, double sampleRate);
+void CLAPRenderBuffer(CLAPPlugIn clapPlugIn, uint32_t blockSize, int64_t samplePosition, float * buffer);
+void CLAPStopRendering(CLAPPlugIn clapPlugIn);
+void CLAPDestroyPlugIn(CLAPPlugIn clapPlugIn);
+void CLAPUnloadBinary(CLAPBinary clapBinary);
+
+#if defined(__cplusplus)
+}   // extern "C"
+#endif
