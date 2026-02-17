000eedd66cf09bf96a6e57ca0962c3986db53f00
sgretscher <41306803+sgretscher@users.noreply.github.com>
2026-02-17 12:26:20 +0100

    ARA_Library and ARA_Examples now consistently require C++14 on all platforms

--- ExamplesCommon/Utilities/StdUniquePtrUtilities.h
+++ ExamplesCommon/Utilities/StdUniquePtrUtilities.h
@@ -80,42 +80,4 @@ inline intptr_t index_of (std::vector<std::unique_ptr<T>> const& container, cons
 
 }   // namespace ARA
 
-/*******************************************************************************/
-// Implement std::make_unique for pre-C++14 compilers, as published by ISO here:
-// https://isocpp.org/files/papers/N3656.txt
-#if !defined (_MSC_VER) && (__cplusplus < 201402L)
-namespace std
-{
-    template<class T>
-    struct _Unique_if
-    {
-        typedef unique_ptr<T> _Single_object;
-    };
-    template<class T>
-    struct _Unique_if<T[]>
-    {
-        typedef unique_ptr<T[]> _Unknown_bound;
-    };
-    template<class T, size_t N>
-    struct _Unique_if<T[N]>
-    {
-        typedef void _Known_bound;
-    };
-
-    template<class T, class... Args>
-    typename _Unique_if<T>::_Single_object make_unique (Args&&... args)
-    {
-        return unique_ptr<T> (new T (std::forward<Args> (args)...));
-    }
-    template<class T>
-    typename _Unique_if<T>::_Unknown_bound make_unique (size_t n)
-    {
-        typedef typename remove_extent<T>::type U;
-        return unique_ptr<T> (new U[n] ());
-    }
-    template<class T, class... Args>
-    typename _Unique_if<T>::_Known_bound make_unique (Args&&...) = delete;
-}   // namespace std
-#endif // !defined (_MSC_VER) && (__cplusplus < 201402L)
-
 #endif // StdUniquePtrUtilities_h
